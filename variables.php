f6f8e383add65fd0c64d1f10371ce0a36d15088e
charls96 <charly.lopez.perez@gmail.com>
2021-08-07 12:31:38 +0200

    first steps and variables

---
+++ variables.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $foo = "bar";
+    echo "$foo </br>";
+    $$foo = "baz";
+    echo $bar;
+
+    echo "</br></br>";
+    $white = "snow";
+    $black =& $white;
+    unset($white);
+    print $black;
+
+    function &retRef(){ // note the &
+
+        $var = "PHP";
+        return $var;
+
+    }
+
+    $v =& retRef(); // note the &
+
+    echo "</br>";
+    print $v;
+    echo "</br>";
+
+    /* function updateCounter(){
+    
+        global $counter;
+        $counter++;
+    }
+    $counter = 10;
+    updateCounter();
+    echo "</br>" . $counter . "</br>";
+    */
+   /*  function updateCounter2()
+    {
+        $GLOBALS[‘counter’]++;
+    }
+    $counter2 = 10;
+    updateCounter2();
+    echo $counter2; */
+
+    function updateCounter()
+    {
+        static $counter = 0;
+        $counter++;
+        echo "Static counter is now {$counter}<br/>";
+    }
+    $counter = 10;
+    updateCounter();
+    updateCounter();
+    echo "Global counter is {$counter} </br>";
+
+    function greet($name)
+    {
+    echo "Hello, {$name}";
+    }
+    greet("Janet");
+
+    ?>
+</body>
+</html>
