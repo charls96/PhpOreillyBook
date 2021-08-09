51a3d0ba20833373896d66a7ca6ec2f4a05110c6
charls96 <charly.lopez.perez@gmail.com>
2021-08-09 12:46:56 +0200

    castings and flow control statements

---
+++ casting.php
@@ -0,0 +1,26 @@
+
+<?php
+
+    $a = "5";
+    $a = (int) $a; // now $a holds an integer
+    
+    ##################
+    class Person
+    {
+        var $name = "Fred";
+        var $age = 35;
+    }
+
+    $o = new Person;
+    $a = (array) $o;
+    print_r($a);
+    echo "</br>";
+    
+    #################
+    $a = array('name' => "Fred", 'age' => 35, 'wife' => "Wilma");
+    $o = (object) $a;
+    echo $o->name;
+
+    $listing = `ls -ls /tmp`;
+    echo $listing;
+?>
---
+++ flow_control_statements.php
@@ -0,0 +1,76 @@
+<?php
+
+    $user_validated = true;
+    $active = true;
+
+    if ($user_validated) {
+        echo "Welcome!";
+        $greeted = 1;
+    }
+    else {
+        echo "Access Forbidden!";
+        exit;
+    }
+?>
+   
+<?php if ($user_validated): ?>
+    <table>
+    <tr>
+    <td>First Name:</td><td>Sophia</td>
+    </tr>
+    <tr>
+    <td>Last Name:</td><td>Lee</td>
+    </tr>
+    </table>
+<?php else: ?>
+    Please log in.
+<?php endif ?>
+
+
+<td><?php if($active) { echo "yes"; } else { echo "no"; } ?></td>
+
+<td><?php echo $active ? "yes" : "no"; ?></td>
+
+<?php
+    $name = "carlos";
+
+    switch ($name) {
+        case 'sylvie': // fall-through
+        case 'bruno':
+        print("yes");
+        break;
+        default:
+        print("no");
+        break;
+    }
+
+    #################
+    $i = 0;
+    $j = 0;
+
+    while ($i < 10) {
+        $i++;
+        while ($j < 10) {
+            if ($j == 5) {
+            continue 2; // continues through two levels
+        }
+        $j++;
+        }
+    }
+    echo "</br>{$i}, {$j}</br>";
+
+    ################
+
+    $total = 0;
+    
+    for ($i= 1; $i <= 10; $i++) {
+        $total += $i;
+    }
+    echo $total;
+
+    for ($i = 0, $j = 1; $i <= 10; $i++, $j *= 2) {
+        $total += $j;
+    }
+    echo "</br>$total";
+
+?>
